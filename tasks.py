fe84c3e1f934423bdc1223c0c5693971e2518c05
Alessandro Molari <molari.alessandro@gmail.com>
2016-02-17 23:26:09 +0000

    Add cleanup and test tasks

--- tasks.py
+++ tasks.py
@@ -1,30 +1,51 @@
 # ------------------------------------------------------------------------------
 # IMPORTS ----------------------------------------------------------------------
 
-import os, sys
-
-import invoke
+from os      import system
+from os      import listdir  as list_dir
+from os.path import basename as base_name
+from os.path import dirname  as dir_name
+from os.path import realpath as real_path
+from os.path import relpath  as rel_path
+from os.path import isfile   as is_file
+from os.path import join     as join_path
+from invoke  import task
 
 # ------------------------------------------------------------------------------
 # CONFIGURATION ----------------------------------------------------------------
 
-ROOTDIR   = os.path.dirname(os.path.realpath(__file__))
-SHAREDDIR = os.path.join(ROOTDIR, 'shared')
+ROOT_DIR   = dir_name(real_path(__file__))
+SHARED_DIR = join_path(ROOT_DIR, 'shared')
+TEST_DIR   = join_path(ROOT_DIR, 'test')
 
 # ------------------------------------------------------------------------------
 # TASKS ------------------------------------------------------------------------
 
-@invoke.task(help={'ctfname': 'CTF name', 'challengename': 'Challenge name'})
-def createchallenge(ctfname, challengename):
+@task(help={'ctf_name': 'CTF name', 'challenge_name': 'Challenge name'})
+def create_challenge(ctf_name, challenge_name):
     """Prepare the storage for a CTF challenge."""
-    chaldir=os.path.join(ROOTDIR, ctfname, challengename)
-    sharedlink=os.path.relpath(SHAREDDIR, chaldir)
+    challenge_dir = join_path(ROOT_DIR, ctf_name, challenge_name)
+    shared_link   = rel_path(SHARED_DIR, challenge_dir)
 
     print('Preparing {storage} for challenge {challenge} of CTF {ctf}'.format(
-        storage=chaldir, challenge=challengename, ctf=ctfname))
+        storage=challenge_dir, challenge=challenge_name, ctf=ctf_name))
 
-    os.makedirs(chaldir)
-    os.symlink(sharedlink, os.path.join(chaldir, 'shared'))
+    os.makedirs(challenge_dir)
+    os.symlink(shared_link, join_path(challenge_dir, 'shared'))
+
+@task
+def test():
+    for file_rel_path in list_dir(TEST_DIR):
+        file_path = join_path(TEST_DIR, file_rel_path)
+        file_name = base_name(file_path)
+        if (is_file(file_path)
+            and file_name.startswith('test_')
+            and file_name.endswith('.py')):
+            system('python {}'.format(file_path))
+
+@task
+def cleanup():
+    system('find . -name "*.pyc" -exec rm {} \;')
 
 # ------------------------------------------------------------------------------
 # vim: set filetype=python :
